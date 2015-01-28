c2b79446f60548893690121c9d7f8cec9f40382a
kmcguire <kmcg3413@gmail.com>
2015-01-28 09:06:01 -0600

    pushing work

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 #![allow(unstable)]
 #![allow(dead_code)]
 #![allow(unused_variables)]
+#![allow(unused_imports)]
 
-
+extern crate cgmath;
 extern crate glutin;
 extern crate glium;
 #[plugin]
@@ -15,51 +16,73 @@ use std::time::duration::Duration;
 use std::io;
 use std::vec::Vec;
 use std::path::Path;
+use std::sync::Arc;
 
-use glium::{Display, Frame, VertexBuffer};
+use glium::{Display, Frame, VertexBuffer, Surface};
 use glium::index_buffer::{IndexBuffer, TrianglesList};
-use glium::vertex;
-use glium::vertex::AttributeType;
+use glium::program::Program;
 
+use cgmath::{Point3, Vector3, Matrix4, Basis3, Matrix3, Quaternion, PerspectiveFov};
+use cgmath::ToMatrix4;
+
+/// Represents an attachment of one object to another.
 struct SkeletonObjectAttachment {
     sobject:        SkeletonObject,
-    l:              [f64; 3],
+    /// location offset from parent object's center + location
+    l:              Vector3<f64>,
 }
 
 #[vertex_format]
 #[derive(Copy)]
 struct Vertex {
     position:   [f32; 3],
-    texcoords:  [f32; 2],
+    color:      [f32; 3],
 }
 
+#[uniforms]
+struct Uniforms {
+    matrix:     [[f32; 4]; 4],
+}
+
+/// Contains state data for game calculations and GL data rendering. The
+/// GL data must be updated from the game state to synchronize the rendering
+/// with the game state.
 struct SkeletonObject {
-    l:              [f64; 3],
-    r:              [f64; 3],
+    /// local location
+    l:              Vector3<f64>,
+    /// local rotation
+    r:              Vector3<f64>,
+    /// local scale
+    s:              Vector3<f64>,
     vertex:         VertexBuffer<Vertex>,
-    attachment:     Vec<SkeletonObjectAttachment>
+    attachment:     Vec<SkeletonObjectAttachment>,
+    /// opengl drawable data
+    drawable:       DrawableObject,
 }
 
+/// An object used directly by the engine to render an object.
 struct DrawableObject {
     name:           String,
     vbuf:           VertexBuffer<Vertex>,
     tlst:           IndexBuffer,
+    uniforms:       Uniforms,
+    programs:       Arc<Program>,
 }
 
 impl DrawableObject {
-    pub fn draw(frame: &Frame) {
-
+    pub fn draw(&self, frame: &mut Frame) {
+        use glium::Surface;
+        frame.draw(&self.vbuf, &self.tlst, &*self.programs, &self.uniforms, &std::default::Default::default()).unwrap();
     }
 
     /// Read file in `obj` format and return a new `Mesh` object.
-    pub fn new_fromobj(display: &Display, source: &str) -> Vec<DrawableObject> {
+    pub fn new_fromobj(display: &Display, source: &str, programs: &Arc<Program>) -> Vec<DrawableObject> {
         let psource = Path::new(source);
         let mut file = io::File::open_mode(&psource, io::Open, io::Read).unwrap();
         let data = file.read_to_string().unwrap();
         let slice = data.as_slice();
-        let objects: Vec<DrawableObject> = Vec::new();
+        let mut objects: Vec<DrawableObject> = Vec::new();
         let mut lines = data.lines();
-        let mut object: Option<DrawableObject> = Option::None; 
         let mut name: Option<String> = Option::None;
         let mut tlst: Vec<u16> = Vec::new();
         let mut vbuf: Vec<Vertex> = Vec::new();
@@ -68,10 +91,12 @@ impl DrawableObject {
             let first = parts.next().unwrap();
             if first.eq("o") {
                 if name.is_some() {
-                    object = Option::Some(DrawableObject {
-                        name:   name.unwrap(),
-                        vbuf:   VertexBuffer::new(display, vbuf),
-                        tlst:   IndexBuffer::new(display, TrianglesList(tlst)),
+                    objects.push(DrawableObject {
+                        name:     name.unwrap(),
+                        vbuf:     VertexBuffer::new(display, vbuf),
+                        tlst:     IndexBuffer::new(display, TrianglesList(tlst)),
+                        uniforms: Uniforms { matrix: [[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]] },
+                        programs: programs.clone(),
                     });
                     tlst = Vec::new();
                     vbuf = Vec::new();
@@ -80,8 +105,7 @@ impl DrawableObject {
                 continue;
             }
 
-            if first.eq("v") && object.is_some() {
-                let obj = object.expect("No Object Found Yet");
+            if first.eq("v") {
                 let v = [
                     parts.next().unwrap().parse::<f64>().unwrap(),
                     parts.next().unwrap().parse::<f64>().unwrap(),
@@ -89,21 +113,19 @@ impl DrawableObject {
                 ];
                 vbuf.push(Vertex {
                     position:    [v[0] as f32, v[1] as f32, v[2] as f32],
-                    texcoords:   [0.0, 0.0],
+                    color:       [1.0, 1.0, 1.0],
                 });
-                object = Option::Some(obj);
                 continue;
             }
 
-            if first.eq("f") && object.is_some() {
-                let obj = object.expect("No Object Found Yet");
-                let a = parts.next().unwrap().parse::<u16>().unwrap();
-                let b = parts.next().unwrap().parse::<u16>().unwrap();
-                let c = parts.next().unwrap().parse::<u16>().unwrap();
+            if first.eq("f") {
+                let a = parts.next().unwrap().parse::<u16>().unwrap() - 1;
+                let b = parts.next().unwrap().parse::<u16>().unwrap() - 1;
+                let c = parts.next().unwrap().parse::<u16>().unwrap() - 1;
                 let d = parts.next();
                 if d.is_some() {
-                    let d = parts.next().unwrap().parse::<u16>().unwrap();
-                    // quad
+                    let d = d.unwrap().parse::<u16>().unwrap() - 1;
+                    // quad (which we turn into two triangles)
                     tlst.push(a);
                     tlst.push(b);
                     tlst.push(c);
@@ -116,10 +138,17 @@ impl DrawableObject {
                     tlst.push(b);
                     tlst.push(c);
                 }
-                object = Option::Some(obj);
                 continue;
             }
         }
+
+        objects.push(DrawableObject {
+            name:     name.unwrap(),
+            vbuf:     VertexBuffer::new(display, vbuf),
+            tlst:     IndexBuffer::new(display, TrianglesList(tlst)),
+            uniforms: Uniforms { matrix: [[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]] },
+            programs: programs.clone(),
+        });
 
         objects
     }
@@ -128,26 +157,68 @@ impl DrawableObject {
 fn main() {
     use glium::DisplayBuild;
     use glium::Surface;
+    use cgmath::FixedArray;
+    use cgmath::BaseFloat;
+    use cgmath::Angle;
 
     let display = glutin::WindowBuilder::new()
         .with_dimensions(360, 360)
         .with_title(format!("Hello World"))
+        .with_depth_buffer(32)
         .build_glium().unwrap();
 
-    DrawableObject::new_fromobj(&display, "test.obj");
+    let program = Arc::new(glium::Program::from_source(&display,
+        // vertex shader
+        "   #version 110
 
-    if true {
-        return;
-    }
+            uniform mat4 matrix;
+
+            attribute vec3 position;
+            attribute vec3 color;
+
+            varying vec3 v_color;
+
+            void main() {
+                gl_Position = vec4(position, 1.0) * matrix;
+                v_color = color;
+            }
+        ",
+
+        // fragment shader
+        "   #version 110
+            varying vec3 v_color;
+
+            void main() {
+                gl_FragColor = vec4(v_color, 1.0);
+            }
+        ",
+
+        // optional geometry shader
+        None
+    ).unwrap());
+
+    let mut objects = DrawableObject::new_fromobj(&display, "test.obj", &program);
+
+    let mut r3: Quaternion<f32> = Quaternion::from_sv(0.0, Vector3::new(0.0, 1.0, 0.0));
 
     loop {
         for event in display.poll_events() {
-            println!("event {:?}", event);
+            //println!("event {:?}", event);
         }
 
         let mut target = display.draw();
 
         target.clear_color(0.0, 0.0, 0.0, 0.0);
+        objects[0].draw(&mut target);
+
+        r3.s += -0.01;
+
+        let m4: Matrix4<f32> = Matrix4::identity();
+        //let m4 = m4 * r3.to_matrix4() * Matrix4::from_translation(&Vector3::new(0.0, 0.0, 1.0));
+        let m4 = m4 * Matrix4::from_translation(&Vector3::new(-20.0, 0.0, 0.0));
+        let per = (PerspectiveFov { fovy: 45.0, aspect: 1.0, near: 0.1, far: 10.0 }).to_matrix4();
+
+        objects[0].uniforms.matrix = m4.into_fixed();
         target.finish();
 
         std::io::timer::sleep(Duration::milliseconds(17));
